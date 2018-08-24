df1d0a8f439b72b72d721a1e2d81d21c6cc34dd4
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-08-24 19:37:11 +0000

    add partials directory, and include in home page

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ var express         = require("express"),
     mongoose        = require("mongoose"),
     flash           = require("connect-flash");
     
+var indexRoutes = require("./routes/index");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
+app.use(indexRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Personal blog has started."); 
