05927896871b3948256f4f712442a0bab6d805c9
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-08-24 19:00:34 +0000

    add initial root route

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express    = require ("express"),
+    router     = express.Router(),
+    User       = require("../models/user"),
+    passport   = require("passport");
+    
+
+
+router.get("/", function(req, res){
+    res.render("home");
+});
