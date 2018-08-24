c3aa0f28fbfa2390d9ad425397d836d6230ab0d2
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-08-24 15:41:03 +0000

    add passport packages, body parser, mongoose and flash

---
+++ app.js
@@ -0,0 +1,20 @@
+var express         = require("express"),
+    app             = express(),
+    ejs             = require("ejs"),
+    passport        = require("passport"),
+    localStrategy   = require("passport-local"),
+    methodOverride  = require("method-override"),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    flash           = require("connect-flash");
+    
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Personal blog has started."); 
+});
