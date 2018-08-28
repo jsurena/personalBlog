992b514cf9ec50342590c5445fb58673115b557d
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-08-28 02:54:11 +0000

    add new route and post route to be used for blog post creation

--- routes/posts.js
+++ routes/posts.js
@@ -5,4 +5,12 @@ router.get("/posts/:id", function(req, res){
    res.render("./posts/show", {id: req.params.id}); 
 });
 
+router.get("/posts/new", function(req, res){
+    res.render("./posts/new");
+});
+
+router.post("/posts", function(req, res){
+    res.send("You have hit the post route");
+});
+
 module.exports = router;
