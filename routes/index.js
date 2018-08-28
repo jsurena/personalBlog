6028fecdb3a9525eb5c94254727c1db3f81fc364
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-08-28 04:06:03 +0000

    add login and register pages as well as forms

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,12 @@ router.get("/about", function(req, res){
    res.render("aboutme"); 
 });
 
+router.get("/login", function(req, res){
+    res.render("login");
+});
+
+router.get("/register", function(req, res){
+    res.render("register");
+});
+
 module.exports = router;
