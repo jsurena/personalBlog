c950ce7f934735abef561cc94e8c379ec4e6f66f
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-08-30 02:46:12 +0000

    add logic to register route to check if passwords match

--- routes/index.js
+++ routes/index.js
@@ -22,15 +22,19 @@ router.get("/register", function(req, res){
 });
 
 router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username, email: req.body.email});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            return res.render("register", {"error": err.message});
-        }
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/");
+    if(req.body.password === req.body.passwordCheck){
+        var newUser = new User({username: req.body.username, email: req.body.email});
+        User.register(newUser, req.body.password, function(err, user){
+            if(err){
+                return res.render("register", {"error": err.message});
+            }
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/");
+            });
         });
-    });
+    } else {
+        return res.render("register", {"error": "Passwords did not match"});
+    }
 });
 
 // LOGGING IN
