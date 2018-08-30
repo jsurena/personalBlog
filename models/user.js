8b3b6a074870992f48078e50818a96c5102ffd16
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-08-30 02:40:10 +0000

    add passport local mongoose functionality to user schema

--- models/user.js
+++ models/user.js
@@ -1,10 +1,13 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = mongoose.Schema({
     username: String,
     password: String,
     email: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 var User = mongoose.model("User", userSchema);
 
