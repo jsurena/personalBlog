ff4912005b609174c6523d1d7a65065725278c40
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-09-01 03:53:33 +0000

    move header's header tag into top of body, add currentUser variable to all pages

--- app.js
+++ app.js
@@ -41,6 +41,11 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.use(indexRoutes);
 app.use(postRoutes);
 
